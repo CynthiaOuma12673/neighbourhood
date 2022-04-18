4eb2c71d50579e4db15379a9137664a2b4f664e2
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-18 16:41:11 +0300

    it will create the admin file and add the classes

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Neighbourhood,Post,Business,Profile
 # Register your models here.
+
+admin.site.register(Neighbourhood),
+admin.site.register(Post),
+admin.site.register(Business),
+admin.site.register(Profile)
