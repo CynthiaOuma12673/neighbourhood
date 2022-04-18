425a235d8c87247f25e4c25d8ff7478bbaf1fe87
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-18 16:50:39 +0300

    it will add imports to the test file

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from .models import Post, Neighbourhood,Business,Profile
+from django.contrib.auth.models import User
+import datetime as dt
 
 # Create your tests here.
